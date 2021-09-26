6897320f23d4ba69cf7198b4a7ad15ae904d5fcf
Albenis Kërqeli <albeniskerqeli5@gmail.com>
2021-09-27 00:51:02 +0200

    Update Final.js

--- src/components/Final.js
+++ src/components/Final.js
@@ -5,16 +5,18 @@ import styling from "../styles/Final.module.css";
 const Final = () => {
   const history = useHistory();
   return (
-    carts !== "" && (
       <Suspense fallback="loading...">
+
+    carts !== "" && (
       <div className={styling.Final}>
         <h1>Congratulations, You just checkout your order</h1>
         <h4>Order delivers shortly, all the best</h4>
         <h4>Order Id : {Math.floor(Math.random() * 100000000)}</h4>
         <button onClick={(e) => history.push("/")}>Return to Homepage</button>
       </div>
-</Suspense>
     )
+    </Suspense>
+
   );
 };
 
